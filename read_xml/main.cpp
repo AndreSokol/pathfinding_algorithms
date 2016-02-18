f98f29a17951a2b6ee8721ea5e50fb1bc5648b7d
Andrey Sokolov <sokolov-as-i@yandex.ru>
2016-02-18 15:48:21 +0300

    more settings to .gitignore

--- read_xml/main.cpp
+++ read_xml/main.cpp
@@ -7,13 +7,13 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     if (argc != 2) {
-        printf("Usage: .exe <filePath>");
+        cerr << "Usage: .exe <filePath>" << endl;
         return 0;
     }
 
     TiXmlDocument doc ( argv[1] );
     if (!doc.LoadFile()) {
-        printf("Error: cannot read xml");
+        cerr << "Error: cannot read xml" << endl;
         return 0;
     }
 
